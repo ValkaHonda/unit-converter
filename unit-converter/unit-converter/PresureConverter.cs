5871894d2e63c9ab2b212a99178bdf65ec1ff473
ValkaHonda <valentin805@gmail.com>
2019-04-14 05:49:52 +0300

    Add Presure & Weight Coverter implementation.

---
+++ unit-converter/unit-converter/PresureConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_converter
+{
+    class PresureConverter : Converter
+    {
+        override public double convert(String from, String to, double value)
+        {
+            double baseValue = getBaseValue(from, value);
+            double convertedValue = convertTo(to, baseValue);
+            return convertedValue;
+
+        }
+        private double getBaseValue(String from, double value)
+        {
+            switch (from)
+            {
+                case "pascal":
+                    return value;
+                case "hectopascal":
+                    return value * 100;
+                case "kilopascal":
+                    return value * 1000;
+                default:
+                    return value;
+            }
+        }
+        private double convertTo(String to, double value)
+        {
+            switch (to)
+            {
+                case "pascal":
+                    return value;
+                case "hectopascal":
+                    return value / 100;
+                case "kilopascal":
+                    return value / 1000;
+                default:
+                    return value;
+            }
+        }
+    }
+}
---
+++ unit-converter/unit-converter/WeightConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_converter
+{
+    class WeightConverter : Converter
+    {
+        override public double convert(String from, String to, double value)
+        {
+            double baseValue = getBaseValue(from, value);
+            double convertedValue = convertTo(to, baseValue);
+            return convertedValue;
+
+        }
+        private double getBaseValue(String from, double value)
+        {
+            switch (from)
+            {
+                case "tons":
+                    return value;
+                case "kilograms":
+                    return value / 1000;
+                case "grams":
+                    return value / 1000000;
+                default:
+                    return value;
+            }
+        }
+        private double convertTo(String to, double value)
+        {
+            switch (to)
+            {
+                case "tons":
+                    return value;
+                case "kilograms":
+                    return value / 1000;
+                case "grams":
+                    return value / 1000000;
+                default:
+                    return value;
+            }
+        }
+    }
+}
